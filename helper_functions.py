60977642024530673083e397bf4afa323465492b
Abdishifa <shifaabdrehim80gmail.com>
2021-01-19 20:09:14 -0500

    playing with docker file

--- helper_functions.py
+++ helper_functions.py
@@ -49,9 +49,25 @@ if __name__ == "__main__":
 #Lets creat class 
 
 class Person:  
-    
-    def __init__(self, name, age):
-    self.name = name
-    self.age = age
 
-    
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+#lets run some code with docker
+
+docker run hello world
+
+### So logging/io works reliably w/Docker
+ENV PYTHONUNBUFFERED=1
+### UTF Python issue for Click package (pipenv dependency)
+ENV LC_ALL=C.UTF-8
+ENV LANG=C.UTF-8
+### Need to explicitly set this so `pipenv shell` works
+ENV SHELL=/bin/bash
+
+### Basic Python dev dependencies
+RUN apt-get update && \
+  apt-get upgrade -y && \
+  apt-get install python3-pip curl -y && \
+  pip3 install pipenv
